55f7ca899c602aed5aa405c05adc3682b6337d77
agexxxecute <167965583+agexxxecute@users.noreply.github.com>
2025-01-12 16:02:30 +0400

    Javadoc update

--- src/main/java/com/petproject/numbergenerateservice/NumberGenerateServiceApplication.java
+++ src/main/java/com/petproject/numbergenerateservice/NumberGenerateServiceApplication.java
@@ -3,6 +3,12 @@ package com.petproject.numbergenerateservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Основной класс микросервиса для генерации номера заказа.
+ * Позволяет сгенерировать номер заказа по заданному алгоритму и сохранить его.
+ *
+ * @author Egor Nazarev
+ */
 @SpringBootApplication
 public class NumberGenerateServiceApplication {
 
